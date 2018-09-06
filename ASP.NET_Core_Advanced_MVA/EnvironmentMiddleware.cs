9b372159b83a8051f8ad4879e22c0432df2af360
Utkarsh Dalwadi <utkarshdalwadi0@gmail.com>
2018-09-06 14:23:39 +0530

    Configure Proj

---
+++ ASP.NET_Core_Advanced_MVA/EnvironmentMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Advanced_MVA
+{
+    public class EnvironmentMiddleware
+    {
+        private readonly RequestDelegate _Next;
+        private IHostingEnvironment _Env;
+        public EnvironmentMiddleware(RequestDelegate next,IHostingEnvironment env)
+        {
+            _Next = next;
+            _Env = env;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            var timer = Stopwatch.StartNew();
+            context.Response.Headers.Add("X-HostingEnviromnentName", new[] { _Env.EnvironmentName }); 
+
+            await _Next(context);
+
+            if(_Env.IsDevelopment()&& context.Response.ContentType!=null &&
+                context.Response.ContentType == "text/html")
+            {
+                await context.Response.WriteAsync($"<p>From {_Env.EnvironmentName} in {timer.ElapsedMilliseconds} ms</p>");
+            }
+        }
+
+    }
+
+    public static class MiddlewareHelpers
+    {
+        public static IApplicationBuilder UseEnvironmentMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<EnvironmentMiddleware>();
+        }
+    }
+
+}
--- ASP.NET_Core_Advanced_MVA/Startup.cs
+++ ASP.NET_Core_Advanced_MVA/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ASP.NET_Core_Advanced_MVA
 {
@@ -18,16 +20,46 @@ namespace ASP.NET_Core_Advanced_MVA
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(LogLevel.Information);
+            var logger = loggerFactory.CreateLogger("Middleware Demo");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                var timer = Stopwatch.StartNew();
+                logger.LogInformation($"==> Beginning request in {env.EnvironmentName}");
+                await next();
+                logger.LogInformation($"==> Complete request in {timer.ElapsedMilliseconds} ms");
+            });
+
+            app.UseEnvironmentMiddleware();
+
+            app.Map("/Contacts", a => a.Run(async context =>
+               {
+                   await context.Response.WriteAsync("Here are your contacts:");
+               }));
+
+            app.MapWhen(context => context.Request.Headers["User-Agent"].First().Contains("Firefox"), FirefoxRoute);
+
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
+                context.Response.ContentType = "text/html";
                 await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        private void FirefoxRoute(IApplicationBuilder obj)
+        {
+            obj.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello Firefox");
             });
         }
     }
